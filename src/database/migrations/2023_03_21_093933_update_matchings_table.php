d0b4bc7dfe018ea32e55d1cdc518a1b48b974584
ruitomo <aria4236@gmail.com>
2023-03-22 14:16:03 +0900

    matchingsテーブルに外部キーを追加しました。

---
+++ src/database/migrations/2023_03_21_093933_update_matchings_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateMatchingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('matchings', function (Blueprint $table) {
+            $table->unsignedBigInteger('from_user_id')->change();
+            $table->unsignedBigInteger('to_user_id')->change();
+            $table->unsignedBigInteger('room_id');
+
+            // Add foreign key constraints
+            $table->foreign('from_user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('to_user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('matchings', function (Blueprint $table) {
+            // Drop foreign key constraints
+            $table->dropForeign(['from_user_id']);
+            $table->dropForeign(['to_user_id']);
+            $table->dropForeign(['room_id']);
+
+            // Drop room_id column
+            $table->dropColumn('room_id');
+        });
+    }
+}
