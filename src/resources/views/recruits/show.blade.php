8d650151b229e3758402f3d3c0a51e869c82542d
ruitomo <aria4236@gmail.com>
2023-03-26 17:09:24 +0900

    応募した時既にマッチングされていた時のエラー表示を実装

--- src/resources/views/recruits/show.blade.php
+++ src/resources/views/recruits/show.blade.php
@@ -1,5 +1,13 @@
 <div class="container">
     <h1>{{ $recruit->headline }}</h1>
+
+    <!-- Error message -->
+    @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+    @endif
+
     <p>サウナ施設名: {{ $recruit->facility }}</p>
     <p>サ飯待ち合わせ時間: {{ $recruit->meeting_time }}</p>
     <p>募集内容: {{ $recruit->recruitment_contents }}</p>
