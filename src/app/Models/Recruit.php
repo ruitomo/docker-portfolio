122a527bcfbf35ec0f99501d3f09c1292b536de3
ruitomo <aria4236@gmail.com>
2023-03-26 17:10:09 +0900

    ルーム名を募集見出しに変更し何のチャットなのかわかりやすくしました。

--- src/app/Models/Recruit.php
+++ src/app/Models/Recruit.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Matching;
 
 class Recruit extends Model
 {
@@ -33,5 +34,15 @@ class Recruit extends Model
     public function user()
     {
         return $this->belongsTo(User::class, 'from_user_id');
+    }
+
+    public function matching()
+    {
+        return $this->hasOne(Matching::class, 'to_user_id', 'from_user_id');
+    }
+
+    public function is_matched()
+    {
+        return Matching::where('to_user_id', $this->from_user_id)->exists();
     }
 }
