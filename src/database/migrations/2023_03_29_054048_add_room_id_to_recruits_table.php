c5aedf02857ea6d0a627ce1c96fd0342e9988faf
ruitomo <aria4236@gmail.com>
2023-03-29 07:34:17 +0900

    マッチング確認機能を実装しました。

---
+++ src/database/migrations/2023_03_29_054048_add_room_id_to_recruits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRoomIdToRecruitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('recruits', function (Blueprint $table) {
+            $table->unsignedBigInteger('room_id')->nullable()->after('to_user_id');
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('recruits', function (Blueprint $table) {
+            $table->dropForeign(['room_id']);
+            $table->dropColumn('room_id');
+        });
+    }
+}
