fbba04babb79522d57e018d43289e33a46e4a86a
ruitomo <aria4236@gmail.com>
2023-05-06 16:51:07 +0900

    ログインエラーメッセージの文章を変更しました。

--- src/resources/lang/ja/auth.php
+++ src/resources/lang/ja/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => '入力された情報が登録した情報と一致しません。',
+    'password' => '入力されたパスワードが正しくありません。',
+    'throttle' => 'ログイン試行回数が多すぎます。:seconds秒後に再試行してください。',
 
 ];
